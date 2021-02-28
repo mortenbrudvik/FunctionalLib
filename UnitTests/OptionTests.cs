6ded665c2ca63a357131d0d8f3e070e552358b5d
Morten Brudvik <morten@brudvik.com>
2021-02-28 21:44:19 +0100

    Added unit tests

--- UnitTests/OptionTests.cs
+++ UnitTests/OptionTests.cs
@@ -1,16 +1,33 @@
-using System;
+using FluentAssertions;
 using Functional;
-using Functional.Option;
 using Xunit;
+
+using static Functional.F;
 
 namespace UnitTests
 {
     public class OptionsTests
     {
         [Fact]
-        public void Test1()
+        public void Match_when_some_value()
         {
+            var result = Parse("1");
 
+            result.Match(() => false, (_) => true)
+                .Should().BeTrue();
         }
+        
+        [Fact]
+        public void Match_when_none_value()
+        {
+            var result = Parse("hello");
+
+            result.Match(() => false, (_) => true)
+                .Should().BeFalse();
+        }
+        
+        Option<int> Parse(string strNumber) 
+            => int.TryParse(strNumber, out var intNumber) 
+                ? Some(intNumber) : None;
     }
 }
