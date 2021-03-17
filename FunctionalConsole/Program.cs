f5a8259a8467ec24e73491a95264570400a5ce1b
Morten Brudvik <morten@brudvik.com>
2021-03-17 09:19:56 +0100

    Added functional C# book link and code repo

--- FunctionalConsole/Program.cs
+++ FunctionalConsole/Program.cs
@@ -4,6 +4,13 @@ namespace FunctionalConsole
 {
     public class Program
     {
+        /// <summary>
+        /// This code is based on the book Functional C# book by Manning.
+        /// https://www.manning.com/books/functional-programming-in-c-sharp
+        /// Code examples from the book can be found on:
+        /// https://github.com/la-yumba/functional-csharp-code
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             Greetings.Run();
