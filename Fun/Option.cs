a0cd8d5835fd2786684a273eae2cb57e256a1360
Morten Brudvik <morten@brudvik.com>
2021-04-18 13:46:26 +0200

    Option take 2

---
+++ Fun/Option.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Fun
+{
+    public class Option<T> : IEquatable<Option<T>>
+    {
+        public Option()
+        {
+            HasValue = false;
+        }
+
+        private Option(T value)
+        {
+            if(value==null)
+                throw new ArgumentNullException(nameof(Value));
+        
+            Value = value;
+            HasValue = true;
+        }
+
+        internal T Value { get; }
+        internal bool HasValue { get; }
+        internal bool HasNoValue => !HasValue;
+
+        public bool Equals(Option<T> other) =>
+            other switch
+            {
+                {HasNoValue: true} when HasNoValue => true,
+                _ when other == null || other.HasNoValue || HasNoValue => false,
+                _ => Value.Equals(other.Value)
+            };
+
+        public override bool Equals(object obj) =>
+            obj is Option<T> other && Equals(other);
+
+        public override int GetHashCode() =>
+            HasValue ? Value.GetHashCode() : 0;
+    }
+}
